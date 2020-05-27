7e5bb8f5fcda01df1badc9721b00aa4fc1f45749
Sofiavaz <sofigimevazquez@hotmail.com>
2020-05-27 16:38:52 -0300

    Intento de actualización

---
+++ tp2/src/main/java/edu/unlu/sdypp/ej2/withsyncronize/Clientes.java
@@ -0,0 +1,13 @@
+package edu.unlu.sdypp.ej2.with;
+
+
+import java.util.ArrayList;
+
+public class Clientes {
+	private ArrayList<Cliente> clientes;
+
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+}
